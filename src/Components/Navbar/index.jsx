a35d139705958ac53876eb8968516ec46c999014
Benjamin Rodriguez <b.esteban.r.v@gmail.com>
2023-09-19 18:09:44 -0300

    [UPDATE] Login component

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -8,11 +8,11 @@ function Navbar() {
             <nav className="max-w-screen-xl mx-auto flex justify-between items-center w-full h-full">
                 <img src={Logo} alt="Blockchain Logo" className="w-14 m-10" />
                 <ul className="flex items-center gap-16 text-lg">
-                    <li className="text-Rojo font-semibold"> <NavLink to="/">Inicio</NavLink></li>
-                    <li> <NavLink to="/contratos">Contratos</NavLink></li>
-                    <li> <NavLink to="/nosotros">Nosotros</NavLink> </li>
+                    <li className="text-Rojo font-semibold"> <NavLink to="#inicio">Inicio</NavLink></li>
+                    <li> <NavLink to="#contratos">Contratos</NavLink> </li>
+                    <li> <NavLink to="#nosotros">Nosotros</NavLink> </li>
                 </ul>
-                <button className="h-10 bg-Rojo text-Blanco px-6 py-2 mx-6 rounded-full">Iniciar Sesión</button>
+                <button className="h-10 bg-Rojo text-Blanco px-6 py-2 mx-6 rounded-full"> <NavLink to="/login"> Iniciar Sesión </NavLink> </button>
             </nav>
         </header>
     )
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,11 +1,11 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
-// import Login from '../Login'
+import Login from '../Login'
 import './index.css'
 
 const AppRutas = () => useRoutes([
-    { path: '/', element: <Home /> }
-    // { path: '/login', element: <Login /> }
+    { path: '/', element: <Home /> },
+    { path: '/login', element: <Login /> }
 ])
 
 
