8255d6c6142af0569b254d01e916824d576d3360
Benjamin Rodriguez <b.esteban.r.v@gmail.com>
2023-11-29 23:13:23 -0300

    [ADD] connect whith solidity function

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -3,6 +3,7 @@ import { AuthProvider } from '../../Context/auth'
 import Home from '../Home'
 import Login from '../Login'
 // import Contrato from '../Contrato'
+import Smart from '../../Components/CreateSmart'
 import Dashboard from '../Dashboard'
 import NotFound from '../NotFound'
 import './index.css'
@@ -16,6 +17,7 @@ function App() {
           <Route path='/Contrato-Inteligente' element={<Home />} />
           <Route path='/Contrato-Inteligente/login' element={<Login />} />
           <Route path="/Contrato-Inteligente/contrato" element={<Dashboard />} />
+          <Route path='/Contrato-Inteligente/smart' element={<Smart />} />
           <Route path='/Contrato-Inteligente/*' element={<NotFound />} />
         </Routes>
       </AuthProvider>
